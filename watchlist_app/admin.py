76896dbc254050eb9c318de9f9514b50e384cd7e
Daniel <daniel.corbellini28@gmail.com>
2024-06-18 21:27:36 -0300

    Making a migration and testing how serialization works

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from watchlist_app.models import Movie
 
-# Register your models here.
+admin.site.register(Movie)
---
+++ watchlist_app/api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from watchlist_app.api.serializers import MovieSerializer
+from watchlist_app.models import Movie
+
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
