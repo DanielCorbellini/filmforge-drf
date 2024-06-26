5caa8d1d581b4e2f9be6b5bdf3b99546b479fbd5
Daniel <daniel.corbellini28@gmail.com>
2024-06-25 21:25:48 -0300

    POST, PUT, DELETE Request

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,8 +1,36 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'name', 'description', 'active']
+
+    def create(self, validated_data):
+        """
+        When is POST it will jump here
+        Create and return a new Movie instance, given the validated data.
+        """
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        When is PUT it will jump here
+        Update and return an existing Movie instance, given the validated data.
+
+        Args:
+            instance: old data value
+            validated_data : new data value
+        """
+        return super().update(instance, validated_data)
+
+
+"""
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get(
+            'description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+"""
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,15 +4,39 @@ from watchlist_app.api.serializers import MovieSerializer
 from watchlist_app.models import Movie
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response()
