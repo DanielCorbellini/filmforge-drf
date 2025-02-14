a2c21aae46b8855489f4ce0e64fa0584becfe1cc
Daniel <daniel.corbellini28@gmail.com>
2025-02-14 16:59:18 -0300

    Add a new model: Review

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from watchlist_app.models import WatchList, StreamPlataform
+from watchlist_app.models import WatchList, StreamPlataform, Review
 
 admin.site.register(WatchList)
 admin.site.register(StreamPlataform)
+admin.site.register(Review)
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class StreamPlataform(models.Model):
@@ -20,3 +21,17 @@ class WatchList(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    rating = models.PositiveBigIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    watchlist = models.ForeignKey(
+        WatchList, on_delete=models.CASCADE, related_name='reviews')
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.rating) + " - " + self.watchlist.title
